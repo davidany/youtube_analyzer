9a273d3cd1f1282390fab97e89b40c93813c8552
david abraham <dave@springbasemedia.com>
2023-07-04 14:04:41 -0400

    get more info from youtube

--- app.py
+++ app.py
@@ -4,5 +4,12 @@ youtube_url = "https://www.youtube.com/watch?v=aywZrzNaKjs"
 
 yt=YouTube(youtube_url)
 
-if(yt):
+try:
+    if(yt):
         print("Title: ", yt.title)
+        print("Title: ", yt.views)
+        print("Title: ", yt.author)
+        print("Title: ", yt.description)
+
+except:
+    print("there was issue connecting")
